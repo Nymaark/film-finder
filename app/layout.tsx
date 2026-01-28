90daf0fdfb77b9be35732707167c8f4c0c69d09f
Nymaark <nymaaark@gmail.com>
2026-01-28 21:48:34 +0100

    Inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Image className="absolute z-[-1]" src="/hero-bg.png" fill={true} style={{objectFit: "cover"}} alt="Hero background image" />
         {children}
       </body>
     </html>
